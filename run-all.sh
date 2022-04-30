7e1c3cd56b727cd1e647976a60fdaefa8b204e84
Emma Guo <jguo69@jhu.edu>
2022-04-30 00:25:08 -0400

    final

---
+++ run-all.sh
@@ -0,0 +1,6 @@
+## Jiahua Guo
+cat mr.data | python map01.py | python reduce01.py
+cat mr.data | python map02.py | python reduce02.py
+cat mr.data | python map03.py | python reduce03.py
+cat mr.data | python map04.py | python reduce04.py
+cat mr.data | python map05.py | python reduce05.py
